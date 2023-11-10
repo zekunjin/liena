1f551fba50649bfd3a11784590f2c1835e642dec
zekunjin <zekun.jin@foxmail.com>
2023-11-10 17:51:04 +0800

    chore: use rayner server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod utils;
-
-use utils::liena;
+use std::process::{Child, Command};
 
 fn main() {
+    let mut child: Child =
+        Command::new("C:\\Users\\zekun.jin\\Documents\\Projects\\liena\\.resources\\rayner.exe")
+            .spawn()
+            .expect("failed to execute process");
+
     tauri::Builder::default()
-        .setup(|app| Ok(liena::setup(app)))
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    child.kill().expect("failed to kill process");
 }
