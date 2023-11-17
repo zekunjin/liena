cff93889e9e9bab47cb8fa43dbfb659151dc7dec
zekunjin <zekun.jin@foxmail.com>
2023-11-17 14:28:56 +0800

    chore: fix build warning

--- src/pages/dashboard/components/outbound-description.vue
+++ src/pages/dashboard/components/outbound-description.vue
@@ -6,15 +6,20 @@ import TrashCan from '~icons/carbon/trash-can'
 import { type RaynerOutbound } from '~/composables/use-rayner'
 import { useRaynerRequest } from '~/composables/use-rayner'
 
-const props = withDefaults(defineProps<{ outbound?: RaynerOutbound }>(), { outbound: {} })
-const emit = defineEmits(['update:outbound'])
+const props = withDefaults(defineProps<{ outbound?: Partial<RaynerOutbound> }>(), { outbound: undefined })
+const emit = defineEmits(['update:outbound', 'afterDelete'])
 const outbound = useVModel(props, 'outbound', emit)
 
 const isSelected = computed(() => props.outbound?.address)
 
-const onDelete = async (data: { address: string }) => {
+const onClose = () => {
+  outbound.value = undefined
+}
+
+const onDelete = async () => {
+  if (!isSelected.value) { return }
   const client = await useRaynerRequest()
-  await client('/outbounds', { method: 'DELETE', body: data })
+  await client('/outbounds', { method: 'DELETE', body: props.outbound })
   emit('afterDelete')
 }
 </script>
@@ -22,7 +27,7 @@ const onDelete = async (data: { address: string }) => {
 <template>
   <div v-if="isSelected" class="flex flex-col relative h-full">
     <div class="flex justify-end">
-      <div class="bg-black/25 text-white/75 hover:text-white hover:bg-black/50 duration-300 transition-all cursor-pointer rounded-lg p-[2px] absolute top-4 right-4" @click="outbound = {}">
+      <div class="bg-black/25 text-white/75 hover:text-white hover:bg-black/50 duration-300 transition-all cursor-pointer rounded-lg p-[2px] absolute top-4 right-4" @click="onClose()">
         <Close />
       </div>
     </div>
@@ -31,7 +36,7 @@ const onDelete = async (data: { address: string }) => {
       {{ outbound }}
     </div>
 
-    <div class="py-3 flex justify-center duration-300 transition-all hover:bg-red-500 hover:text-white cursor-pointer bg-gray-100" @click="onDelete({ address })">
+    <div class="py-3 flex justify-center duration-300 transition-all hover:bg-red-500 hover:text-white cursor-pointer bg-gray-100" @click="onDelete()">
       <TrashCan />
     </div>
   </div>
